b9e45adefd2dc0df38144e851f699339ed15f57f
Marouan Lagrich <marwanlagrich@gmail.com>
2024-08-12 14:00:03 +0100

    Auto commit

--- youtube.py
+++ youtube.py
@@ -2,7 +2,7 @@ import yt_dlp
 import os
 
 # Specify the YouTube video URL
-video_url = 'https://www.youtube.com/watch?v=OL-CP7ErBAI'
+video_url = 'https://www.youtube.com/watch?v=ON7TMT9vNMI'
 
 # Create an instance of yt-dlp with the desired options
 ydl_opts = {

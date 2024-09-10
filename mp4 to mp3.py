afdc5d52b191840e01ae53b81c1a90849f0e9cae
Marouan Lagrich <marwanlagrich@gmail.com>
2024-09-10 14:00:03 +0100

    Auto commit

--- mp4 to mp3.py
+++ mp4 to mp3.py
@@ -15,9 +15,24 @@ def convert_mp4_to_mp3(mp4_file, mp3_file):
     video.close()
     audio.close()
 
+def mp3_to_wav(mp3_file, wav_file):
+    # Load the video file
+    audio = AudioFileClip(mp3_file)
+
+    # Extract the audio
+    audio = audio.subclip(0.5, audio.duration)
+
+    # Write the audio to a file
+    audio.write_audiofile(wav_file)
+    # Close the video and audio clips
+    audio.close()
+
 
 if __name__ == "__main__":
     # Example usage
     mp4_file = r"C:\Users\black\Downloads\Video/gvif.mp4"
-    mp3_file = r"C:\Users\black\Downloads\Video/audio.mp3"
-    convert_mp4_to_mp3(mp4_file, mp3_file)
+    mp3_file = r"C:\Users\black2\Desktop\pygame/sol-101774.mp3"
+    wav_file = r"C:\Users\black2\Desktop\pygame/sol-101774.wav"
+
+    #convert_mp4_to_mp3(mp4_file, mp3_file)
+    mp3_to_wav(mp3_file, wav_file)
